8f4dd82c9f6de0d9e00fec415bca8d8cd55099e2
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-06-23 13:49:00 +0300

    add OTLP metrics option

--- qmetrics/option.go
+++ qmetrics/option.go
@@ -1,9 +1,17 @@
 package qmetrics
 
+import "context"
+
 type Option func(t *Metric) error
 
 func WithPrometheus(port int) Option {
 	return func(m *Metric) error {
 		return m.prometheusExporter(port)
+	}
+}
+
+func WithOTLP(endPoint string) Option {
+	return func(t *Metric) error {
+		return t.otlpExporter(context.Background(), endPoint)
 	}
 }
