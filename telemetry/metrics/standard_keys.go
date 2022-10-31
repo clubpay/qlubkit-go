b8423e8909555c765d049a69e4b22cda5598cfb4
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-10-31 11:11:46 +0300

    add standard metrics keys

---
+++ telemetry/metrics/standard_keys.go
@@ -0,0 +1,9 @@
+package qmetrics
+
+const (
+	QlubHttpRequestCnt       = "http.requests"
+	QlubHttpResponseTimeHist = "http.response.time"
+	QlubPaymentSuccess       = "qlub.payment.success"
+	QlubPaymentAmountBill    = "qlub.payment.amount.bill"
+	QlubPaymentAmountTip     = "qlub.payment.amount.tip"
+)
